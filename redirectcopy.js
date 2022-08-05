a12d9292ea49174c08acae35dce065b930e014cd
honguyenhanh91 <110671007+honguyenhanh91@users.noreply.github.com>
2022-08-05 21:18:53 +0700

    Add files via upload

---
+++ redirectcopy.js
@@ -0,0 +1,17 @@
+function FSAddOriginalLink(){
+    var body_element = document.getElementsByTagName('body')[0];
+    var selection;
+    selection = window.getSelection();
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://www.facebook.com/Phan-H%C3%B9ng-C%C6%B0%E1%BB%9Dng-1392019657544261'>https://www.facebook.com/Phan-H%C3%B9ng-C%C6%B0%E1%BB%9Dng-1392019657544261</a>";
+    var copytext = pagelink;
+    var newdiv = document.createElement('div');
+    newdiv.style.position='absolute';
+    newdiv.style.left='-99999px';
+    body_element.appendChild(newdiv);
+    newdiv.innerHTML = copytext;
+    selection.selectAllChildren(newdiv);
+    window.setTimeout(function() {
+        body_element.removeChild(newdiv);
+    },0);
+}
+document.oncopy = FSAddOriginalLink;
